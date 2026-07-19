0a97ed5f406b18a47aa62cf104ea8c65d0e7ccfe
MD. Tanzim Rahman Udoy <mrtanzimrahmanuiu50@gmail.com>
2026-07-19 09:47:58 +0600

    Add files via upload

---
+++ arryprin.java
@@ -0,0 +1,18 @@
+public class arryprin {
+    public static void main(String[] args) {
+        //arry Declaration
+        int arr[] = new int[5];
+        int arr1[]= {1,2,3,4,5};
+        int arr2[]= new int[]{6,7,8,9,10};
+        //printing the arry 
+
+        for(int i=0;i<arr1.length;i++)
+        {
+        
+            System.out.print(arr1[i] +" ");
+        }
+        
+
+    }
+    
+}
